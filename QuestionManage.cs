3d33a58b6c012b58fa9f0c081b3e0b6ef1dc266a
pantsugod1976 <57825526+pantsugod1976@users.noreply.github.com>
2023-04-20 15:06:29 +0700

    Update QuestionManage.cs

--- QuestionManage.cs
+++ QuestionManage.cs
@@ -62,7 +62,7 @@ namespace BTL_update
             {
                 try
                 {
-                    MessageBox.Show(connect.server + " " + connect.database);
+                    
                     conn.Open();
                 }
                 catch(Exception ex) 
